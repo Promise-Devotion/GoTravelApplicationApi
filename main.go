0f5d1b608fccf4dbd9180521d3f00ccb21ed2fbe
Promise-Devotion <sijun0224@msn.com>
2023-10-30 22:47:54 +0800

    配置gin-swagger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	docs "github.com/Promise-Devotion/GoTravelApplicationApi/docs"
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
 
 func main() {
-	fmt.Println("hello, world!")
+	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
